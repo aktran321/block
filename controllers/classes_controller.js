7b77fb9a98328a4c106cb841186e7d0148c7b4b9
Emma Stotz <stotz.emma@gmail.com>
2019-11-07 10:33:12 -0500

    Edits to handlebars. Created simple GET routes so that we could see all the pages when we type them into the serach bar.

--- controllers/classes_controller.js
+++ controllers/classes_controller.js
@@ -1,5 +1,10 @@
+// ====================================================
+// DEPENDENCIES
+// ====================================================
 const db = require("../models");
-
+// ====================================================
+// ROUTES (GET ONLY)
+// ====================================================
 module.exports = function(app) {
   app.get("/", function(req, res) {
     db.Classes.findAll({}).then(function(result) {
@@ -12,4 +17,48 @@ module.exports = function(app) {
     });
   });
 
-}
+  app.get("/class-input", function(req, res) {
+    db.Classes.findAll({}).then(function(result) {
+      var classData = {
+        classes: result
+      };
+      res.render("classInput", classData);
+    }).catch(function(err) {
+      res.json(400,err);
+    });
+  });
+
+  app.get("/new-user", function(req, res) {
+    db.Classes.findAll({}).then(function(result) {
+      var classData = {
+        classes: result
+      };
+      res.render("newUser", classData);
+    }).catch(function(err) {
+      res.json(400,err);
+    });
+  });
+
+  app.get("/schedule-display", function(req, res) {
+    db.Classes.findAll({}).then(function(result) {
+      var classData = {
+        classes: result
+      };
+      res.render("scheduleDisplay", classData);
+    }).catch(function(err) {
+      res.json(400,err);
+    });
+  });
+
+  app.get("/user-dashboard", function(req, res) {
+    db.Classes.findAll({}).then(function(result) {
+      var classData = {
+        classes: result
+      };
+      res.render("userDashboard", classData);
+    }).catch(function(err) {
+      res.json(400,err);
+    });
+  });
+
+};
